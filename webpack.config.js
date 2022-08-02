4512802e8b2054e28cc5ae358dcd51256b572192
Zappedfish <zappedfish@gmail.com>
2022-08-02 15:28:35 +0400

    2.08.2022 15:28

--- webpack.config.js
+++ webpack.config.js
@@ -37,6 +37,10 @@ module.exports = {
                     "sass-loader",
                 ],
             },
+            {
+                test: /\.html$/i,
+                loader: "html-loader",
+            },
         ]
     },
     devServer: {
